a308ddb4e68aecfcbd81410714e957de50c175b6
james lee <90737354+Lxkaaaaaa@users.noreply.github.com>
2022-10-24 10:41:51 +0800

    Phong着色器

--- PhongShader.h
+++ PhongShader.h
@@ -7,12 +7,13 @@ struct PhongShader : public IShader
 {
 	mat<2, 3> varyingUV;
 	mat<4, 4> mvp;
-	mat<4, 4> mvpIT;  //mvp±ä»»¾ØÕóµÄÄæ×ªÖÃ¾ØÕó
+	mat<4, 4> mvpIT;  //mvpÂ±Ã¤Â»Â»Â¾Ã˜Ã•Ã³ÂµÃ„Ã„Ã¦Ã—ÂªÃ–ÃƒÂ¾Ã˜Ã•Ã³
 
 	virtual vec4 vertex(int iface, int nthvert);
 
 	virtual bool fragment(const vec3 bar, TGAColor& color);
 };
+
 
 #endif // !_PHONGSHADER_H_
 
